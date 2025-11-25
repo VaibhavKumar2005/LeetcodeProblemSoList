a20d31fa82f7c0d61aa29e9947c810f8cd65f7b1
Vaibhav Kumar <vaibhav.kumar_cs.aiml24@gla.ac.in>
2025-11-25 10:01:33 +0530

    Update 121.best-time-to-buy-and-sell-stocks.java

--- LeetCode Java/121.best-time-to-buy-and-sell-stocks.java
+++ LeetCode Java/121.best-time-to-buy-and-sell-stocks.java
@@ -14,3 +14,21 @@ class Solution {
        return maxProfit;
     }
 }
+
+
+//2nd Code
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxpr = 0;
+        int currpr = 0;
+        int buy = prices[0];
+        for(int i = 0; i < prices.length; i++){
+            buy = Math.min (buy, prices[i]);
+            currpr = prices[i] - buy;
+            maxpr = Math.max (currpr, maxpr);
+
+        }
+        return maxpr;
+    }
+}
