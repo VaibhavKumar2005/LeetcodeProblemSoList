2c73e19d9dcc5d14ba383e68fcf4d73ae2b0038b
Vaibhav Kumar <vaibhav.kumar_cs.aiml24@gla.ac.in>
2026-01-02 23:14:55 +0530

    Add multiply method for string representation of numbers
    Implement string multiplication for two non-negative integers.

--- LeetCode Java/0043.multiply-strings.java
+++ LeetCode Java/0043.multiply-strings.java
@@ -1,1 +1,39 @@
+class Solution {
+    public String multiply(String num1, String num2) {
 
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+
+        int m = num1.length();
+        int n = num2.length();
+
+        int[] result = new int[m + n];
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                int digit1 = num1.charAt(i) - '0';
+                int digit2 = num2.charAt(j) - '0';
+
+                int multiply = digit1 * digit2;
+
+                int position = i + j + 1;
+
+                int sum = multiply + result[position];
+
+                result[position] = sum % 10;       
+                result[position - 1] += sum / 10;  
+            }
+        }
+
+        StringBuilder answer = new StringBuilder();
+        for (int num : result) {
+            if (!(answer.length() == 0 && num == 0)) {
+                answer.append(num);
+            }
+        }
+
+        return answer.toString();
+    }
+}
