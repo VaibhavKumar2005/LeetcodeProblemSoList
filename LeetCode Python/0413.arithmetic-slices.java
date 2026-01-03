ede858587d768531d75df2a63d1a6a7531e21a61
Vaibhav Kumar <vaibhav.kumar_cs.aiml24@gla.ac.in>
2026-01-03 12:59:37 +0530

    Add solution for counting arithmetic slices
    Implement a solution to count arithmetic slices in a list.

---
+++ LeetCode Python/0413.arithmetic-slices.java
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        if len(nums)<3:
+            return 0
+
+        count = 0
+        current = 0
+
+        for i in range(2, len(nums)):
+            if nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]:
+                current = current + 1
+                count = count + current
+
+            else:
+                current = 0
+
+        return count
