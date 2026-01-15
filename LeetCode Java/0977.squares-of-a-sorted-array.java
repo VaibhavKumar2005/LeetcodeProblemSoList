3aef124e4979739a7e6d8dadf08e79c3488915b4
Vaibhav Kumar <vaibhav.kumar_cs.aiml24@gla.ac.in>
2026-01-15 15:28:06 +0530

    Create 0977.squares-of-a-sorted-array.java

---
+++ LeetCode Java/0977.squares-of-a-sorted-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+
+        int[] result = new int[nums.length];
+
+        int left = 0;
+        int right = nums.length - 1;
+        int pos = nums.length - 1;
+
+        while (left <= right) {
+
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[pos] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[pos] = nums[right] * nums[right];
+                right--;
+            }
+
+            pos--;
+        }
+
+        return result;
+    }
+}
