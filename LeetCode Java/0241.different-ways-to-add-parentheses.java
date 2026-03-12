34d5d9f50eb198671bf1a970705883cc8e7f72ff
Vaibhav Kumar <vaibhav.kumar_cs.aiml24@gla.ac.in>
2026-03-12 23:55:40 +0530

    Create 0241.different-ways-to-add-parentheses.java

---
+++ LeetCode Java/0241.different-ways-to-add-parentheses.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public List<Integer> diffWaysToCompute(String expression) {
+
+        List<Integer> result = new ArrayList<>();
+
+        for(int i = 0; i < expression.length(); i++) {
+
+            char c = expression.charAt(i);
+
+            if(c == '+' || c == '-' || c == '*') {
+
+                String left = expression.substring(0, i);
+                String right = expression.substring(i + 1);
+
+                List<Integer> leftResults = diffWaysToCompute(left);
+                List<Integer> rightResults = diffWaysToCompute(right);
+
+                for(int l : leftResults) {
+                    for(int r : rightResults) {
+
+                        if(c == '+') result.add(l + r);
+                        else if(c == '-') result.add(l - r);
+                        else result.add(l * r);
+                    }
+                }
+            }
+        }
+
+        if(result.isEmpty()) {
+            result.add(Integer.parseInt(expression));
+        }
+
+        return result;
+    }
+}
